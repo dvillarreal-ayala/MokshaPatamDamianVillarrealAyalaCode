9c6f3ca56f2a50ed17696c91edfd714eea0be52a
damianayala <damian.villarreal-ayala@menloschool.org>
2024-09-09 12:42:00 -0700

    Work from Saturday/Sunday

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: Damian Villarreal-Ayala
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date        |  Time  |                                     Update |
+|:------------|:------:|-------------------------------------------:|
+| September 4 | 9-10pm | I tried to write abstract, efficient code. |
+| September 8 | 5-6pm  |                                            |
+|             |        |                                            |
 
-
+ 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Damian Villarreal-Ayala
+ * for AT Computer Science: Adventures in Algorithms
  *
  */
 
@@ -14,7 +19,39 @@ public class MokshaPatam {
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
+    public static int snakesAndLadders(int boardsize, int[][] snakes, int[][] ladders)
+    {
+        int[] paths = new int[boardsize];
+        return -1;
+    }
+
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        Queue<Integer> queue = new LinkedList<>();
+        //Starting position
+        queue.add(0);
+
+        boolean[] visited = new boolean[boardsize];
+
+        int numMoves = 0;
+
+        //Continue running until path to the end is finished
+        while(!queue.isEmpty())
+        {
+            int queueSize = queue.size();
+
+            while(queueSize > 0)
+            {
+                queueSize = queueSize - 1;
+                int currentSpace = queue.remove();
+
+                if(currentSpace == boardsize - 1)
+                {
+                    return numMoves;
+                }
+            }
+        }
+
+
 
         return 0;
     }
